8cf38152528c79f8aa9c937e843185971585881c
ultimathei <mmdrummer@gmail.com>
2021-02-05 21:20:57 +0000

    initial piano styling

--- src/scripts/components/App.vue
+++ src/scripts/components/App.vue
@@ -1,14 +1,35 @@
 <template>
   <div id="app">
     <test></test>
+    <div class="piano">
+      <div class="piano__side-panel"></div>
+      <div class="piano__main">
+        <div class="piano__top-panel"></div>
+        <div class="piano__keyboard | piano-keyboard">
+          <div class="piano-keyboard__white-keys">
+            <piano-white-key
+              v-for="n in 8"
+              :key="n"
+              class="piano-keyboard__white-key"
+            ></piano-white-key>
+          </div>
+          <div class="piano-keyboard__black-keys">
+            <div class="piano-keyboard__black-key">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import test from './test/Test.vue';
+import test from "./test/Test.vue";
+import PianoWhiteKey from "./piano/PianoWhiteKey.vue";
 
 export default {
-  components: { test },
+  components: { test, PianoWhiteKey },
   name: "app",
 };
 </script>
---
+++ src/scripts/components/piano/PianoWhiteKey.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :class="['|', 'white-key']">
+    <div class="white-key__top"></div>
+    <div class="white-key__front"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WhiteKey",
+};
+</script>
