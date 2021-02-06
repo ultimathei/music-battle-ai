208f44a6bf776d178377b00833e75bd5084893cc
ultimathei <mmdrummer@gmail.com>
2021-02-06 17:48:50 +0000

    further colors, pressed state style

--- src/scripts/components/piano/PianoBlackKey.vue
+++ src/scripts/components/piano/PianoBlackKey.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="['|', 'black-key']">
     <div class="black-key__top"></div>
-    <div class="black-key__front"></div>
+    <!-- <div class="black-key__front"></div> -->
   </div>
 </template>
 
